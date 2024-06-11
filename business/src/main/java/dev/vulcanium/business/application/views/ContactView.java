55fdffbc10229332d7442c076d2db1751d9f002c
Shadowfflare <139015554+Shadowfflare@users.noreply.github.com>
2024-06-11 21:35:53 +0200

    migrate to vaadin

---
+++ business/src/main/java/dev/vulcanium/business/application/views/ContactView.java
@@ -0,0 +1,10 @@
+package dev.vulcanium.business.application.views;
+
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "contact")
+@PageTitle("ContactForm")
+public class ContactView{
+
+}
---
+++ business/src/main/java/dev/vulcanium/business/application/views/MainView.java
@@ -0,0 +1,11 @@
+package dev.vulcanium.business.application.views;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "")
+@PageTitle("Home")
+public class MainView extends VerticalLayout{
+
+}
